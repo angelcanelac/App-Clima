763f18dc3b71a23b6bbb01f3de03c35e97e339ca
Angel Canela <angel.canela99@gmail.com>
2025-07-09 19:53:50 -0400

    fix

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -17,7 +17,7 @@
             
             try {
                 // Obtener la latitud y longitud de la ciudad
-                const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+                const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
                 const {data} = await axios(url);
                 const {lat, lon} = data[0];
 
